757375d776976774f8bc6c55965b965f066e55c4
wentzpatricia <patriciawentzm@gmail.com>
2023-04-19 21:19:09 -0300

    adicionando variaveis de estilo

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import GlobalStyle from "./style/GlobalStyle";
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/style/GlobalStyle.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+import { colorsVariables } from "./VariablesStyle";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: ${colorsVariables.white};
+  color: ${colorsVariables.black};
+  min-height: 100vh;
+  font-family: "Quicksand", sans-serif;
+}
+
+button {
+  cursor: pointer;
+}
+
+ul,
+nav,
+a,
+li {
+  text-decoration: none;
+  list-style: none;
+}`;
+
+export default GlobalStyle;
