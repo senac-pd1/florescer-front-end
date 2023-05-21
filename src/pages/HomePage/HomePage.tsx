f219c029bfbb3e0c1811c4735fbb0a2a3fc0c2a9
Jorge Henrique Silva Saldanha <50343325+Joorgenho@users.noreply.github.com>
2023-05-21 17:55:15 -0300

    Foi desenvolvida a barra de pesquisa, listagem de 8 plantas para exib… (#4)
    * Foi desenvolvida a barra de pesquisa, listagem de 8 plantas para exibição com botão de like
    
    * Correções e Criação Modal
    
    * Correções e modal
    
    * Instalação do json-serve

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import About from "../../containers/About/About";
 import Banner from "../../containers/Banner/Banner";
 import Billboard from "../../containers/Billboard/Billboard";
 import Header from "../../containers/Header/Header";
+import PlantListing from "../../containers/FlowerListing/PlantListing";
 import { ContainerHomePage } from "./HomePageStyle";
 
 const HomePage = () => {
@@ -11,6 +12,7 @@ const HomePage = () => {
       <Banner />
       <About />
       <Billboard />
+      <PlantListing />
     </ContainerHomePage>
   );
 };
