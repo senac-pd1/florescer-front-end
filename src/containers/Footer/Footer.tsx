11cf6dee8bc86eee7e6caa193a842a0c09e122ab
wentzpatricia <patriciawentzm@gmail.com>
2023-07-09 01:40:43 -0300

    adicionando footer

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -1,5 +1,22 @@
+import { ContainerFooter, ContainerImage, ContainerText } from "./FooterStyle";
+import logo from "./../../assets/logoLogin.svg";
 const Footer = () => {
-  return <h2>Footer</h2>;
+  return (
+    <ContainerFooter>
+      <ContainerImage>
+        <img src={logo} alt="imagem do logo da marca" />
+        <p>
+          Abrace a natureza em suas mãos e cultive um jardim de bem-estar.
+          Cuidados diários, crescimento exuberante e alegria florescendo em cada
+          planta.
+        </p>
+      </ContainerImage>
+      <ContainerText>
+        <h2>Projeto de Desenvolvimento I</h2>
+        <h3>Centro universitário Senac</h3>
+      </ContainerText>
+    </ContainerFooter>
+  );
 };
 
 export default Footer;
